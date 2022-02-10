2e143798b4847de6a85f381fc76bb7c928591183
ananthasankar98 <93582748+ananthasankar98@users.noreply.github.com>
2022-02-11 00:48:17 +0530

    Frontend

---
+++ src/components/about/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './About.css';
+import myImage from './hat.jpg';
+
+function About(props) {
+    return (
+        <h1></h1>
+    );
+}
+
+export default About;
---
+++ src/components/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+function Home(props) {
+    return (
+        <div className='home'>
+            <h1>Publish your passions in your way.<br></br>Create and Stay Unique.</h1>
+        </div>
+    );
+}
+
+export default Home;
